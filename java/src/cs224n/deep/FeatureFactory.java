9d5689cf029c5628ea7c59052f97c8708bd35f8e
eltshan <wu.shen.eltshan@gmail.com>
2015-12-03 23:05:56 -0800

    finished simple dl

--- java/src/cs224n/deep/FeatureFactory.java
+++ java/src/cs224n/deep/FeatureFactory.java
@@ -1,79 +1,128 @@
 package cs224n.deep;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import org.ejml.simple.*;
 
-
 public class FeatureFactory {
 
+	public SimpleMatrix OOV;
 
 	private FeatureFactory() {
 
 	}
 
-	 
 	static List<Datum> trainData;
+
 	/** Do not modify this method **/
 	public static List<Datum> readTrainData(String filename) throws IOException {
-        if (trainData==null) trainData= read(filename);
-        return trainData;
+		if (trainData == null)
+			trainData = read(filename);
+		return trainData;
 	}
-	
+
 	static List<Datum> testData;
+
 	/** Do not modify this method **/
 	public static List<Datum> readTestData(String filename) throws IOException {
-        if (testData==null) testData= read(filename);
-        return testData;
+		if (testData == null)
+			testData = read(filename);
+		return testData;
 	}
-	
-	private static List<Datum> read(String filename)
-			throws FileNotFoundException, IOException {
-	    // TODO: you'd want to handle sentence boundaries
+
+	private static List<Datum> read(String filename) throws FileNotFoundException, IOException {
+		// TODO: you'd want to handle sentence boundaries
+		// TODO: test later
 		List<Datum> data = new ArrayList<Datum>();
 		BufferedReader in = new BufferedReader(new FileReader(filename));
+		data.add(new Datum("<s>", "O"));
 		for (String line = in.readLine(); line != null; line = in.readLine()) {
+
 			if (line.trim().length() == 0) {
+				if (data.get(data.size() - 1).word.equals("<s>"))
+					continue;
+
+				data.add(new Datum("</s>", "O"));
+				data.add(new Datum("<s>", "O"));
 				continue;
 			}
 			String[] bits = line.split("\\s+");
-			String word = bits[0];
+			String word = bits[0].toLowerCase();
+			if (word.equals("-docstart-"))
+				continue;
 			String label = bits[1];
 
 			Datum datum = new Datum(word, label);
 			data.add(datum);
 		}
-
+		if (data.get(data.size() - 1).equals("<s>")) {
+			data.remove(data.size() - 1);
+		}
+		if (!data.get(data.size() - 1).equals("</s>")) {
+			data.add(new Datum("</s>", "O"));
+		}
+		in.close();
 		return data;
 	}
- 
- 
-	// Look up table matrix with all word vectors as defined in lecture with dimensionality n x |V|
-	static SimpleMatrix allVecs; //access it directly in WindowModel
+
+	// Look up table matrix with all word vectors as defined in lecture with
+	// dimensionality n x |V|
+	static SimpleMatrix allVecs; // access it directly in WindowModel
+
 	public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
-		if (allVecs!=null) return allVecs;
-		return null;
-		//TODO implement this
-		//set allVecs from filename		
+		if (allVecs != null)
+			return allVecs;
+		File f = new File(vecFilename);
+		int numOfRows = 100232;// (int) f.length();
+		int numOfCols = 50;
+		System.out.println(numOfRows + " " + numOfCols);
+		BufferedReader br = new BufferedReader(new FileReader(vecFilename));
+
+		double[][] tmpMatrixs = new double[numOfRows + 1][numOfCols];
+		int row = 0;
+		for (String line = br.readLine(); line != null; line = br.readLine()) {
+
+			String[] nums = line.split("\\s+");
+
+			for (int col = 0; col < numOfCols; col++) {
+				tmpMatrixs[row][col] = Double.parseDouble(nums[col]);
+			}
+			row++;
+		}
+
+		for (int i = 0; i < 50; i++) {
+			tmpMatrixs[numOfRows][i] = new Random().nextDouble();
+		}
+
+		br.close();
+		allVecs = new SimpleMatrix(tmpMatrixs);
+		return allVecs;
 
 	}
-	// might be useful for word to number lookups, just access them directly in WindowModel
-	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>(); 
+
+	// might be useful for word to number lookups, just access them directly in
+	// WindowModel
+	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>();
 	public static HashMap<Integer, String> numToWord = new HashMap<Integer, String>();
 
 	public static HashMap<String, Integer> initializeVocab(String vocabFilename) throws IOException {
-		//TODO: create this
+
+		int index = 0;
+		BufferedReader br = new BufferedReader(new FileReader(vocabFilename));
+		String line;
+		while ((line = br.readLine()) != null) {
+			wordToNum.put(line, index++);
+			numToWord.put(index, line);
+		}
+		br.close();
+		// TODO: create this
+		wordToNum.put("OOV", index++);
+		numToWord.put(index, "OOV");
 		return wordToNum;
 	}
- 
-
-
-
-
-
-
-
 
 }
--- java/src/cs224n/deep/WindowModel.java
+++ java/src/cs224n/deep/WindowModel.java
@@ -1,46 +1,245 @@
 package cs224n.deep;
+
+import java.io.IOException;
 import java.lang.*;
 import java.util.*;
 
 import org.ejml.data.*;
 import org.ejml.simple.*;
 
-
 import java.text.*;
 
 public class WindowModel {
 
-	protected SimpleMatrix L, W, Wout;
+	protected SimpleMatrix L, W, Wout, U, b1, b2;
 	//
-	public int windowSize,wordSize, hiddenSize;
+	public int windowSize, wordSize, hiddenSize, K;
 
-	public WindowModel(int _windowSize, int _hiddenSize, double _lr){
-		//TODO
+	private static final String[] LABELS = { "O", "LOC", "MISC", "ORG", "PER" };
+	public double lr;
+	private static HashMap<String, Integer> labels = new HashMap<String, Integer>();
+
+	public WindowModel(int _windowSize, int _hiddenSize, double _lr) {
+		windowSize = 3;
+		wordSize = 50;
+		K = 5;
+		hiddenSize = _hiddenSize;
+		lr = _lr;
 	}
 
 	/**
-	 * Initializes the weights randomly. 
+	 * Initializes the weights randomly.
 	 */
-	public void initWeights(){
-		//TODO
-		// initialize with bias inside as the last column
-	        // or separately
-		// W = SimpleMatrix...
-		// U for the score
-		// U = SimpleMatrix...
-	}
+	public void initWeights() {
+		labels.put("O", 0);
+		labels.put("LOC", 1);
+		labels.put("MISC", 2);
+		labels.put("ORG", 3);
+		labels.put("PER", 4);
 
+		W = SimpleMatrix.random(hiddenSize, windowSize * wordSize, -1, 1, new Random());
+		U = SimpleMatrix.random(K, hiddenSize, -1, 1, new Random());
+
+		b1 = new SimpleMatrix(hiddenSize, 1);
+		b2 = new SimpleMatrix(K, 1);
+	}
 
 	/**
-	 * Simplest SGD training 
+	 * Simplest SGD training
 	 */
-	public void train(List<Datum> _trainData ){
-		//	TODO
+	public void train(List<Datum> _trainData) {
+		double lambda = 1e-4;
+		for (int i = 1; i < _trainData.size() - 1; i++) {
+			SimpleMatrix x = getTrainingWinodw(_trainData, i);
+
+			SimpleMatrix h = tanh(W.mult(x).plus(b1));
+			SimpleMatrix p = softmax(U.mult(h).plus(b2));
+
+			SimpleMatrix y = new SimpleMatrix(K, 1);
+			y.set(labels.get(_trainData.get(i).label), 0, 1);
+			SimpleMatrix diff_p_y = p.minus(y);
+			SimpleMatrix db2 = p.minus(y);
+
+			SimpleMatrix dU = diff_p_y.mult(h.transpose());
+			dU.plus(lambda, U);
+
+			SimpleMatrix db1 = derivativeTanh(h).elementMult(U.transpose().mult(diff_p_y));
+
+			SimpleMatrix dW = db1.mult(x.transpose());
+			dW.plus(lambda, W);
+
+			SimpleMatrix dx = W.transpose().mult(db1);
+
+			b2 = b2.plus(-lr, db2);
+			U = U.plus(-lr, dU);
+			b1 = b1.plus(-lr, db1);
+			W = W.plus(-lr, dW);
+			x = x.plus(-lr, dx);
+
+			/////////
+			updateWordVectors(_trainData, i, x);
+		}
 	}
 
-	
-	public void test(List<Datum> testData){
-		// TODO
+	public void train2(List<Datum> _trainData) {
+		double lambda = 1e-4;
+		for (int i = 1; i < _trainData.size() - 1; i++) {
+			SimpleMatrix x = getTrainingWinodw(_trainData, i);
+
+			SimpleMatrix h = tanh(W.mult(x).plus(b1));
+			SimpleMatrix p = softmax(U.mult(h).plus(b2));
+
+			SimpleMatrix y = new SimpleMatrix(K, 1);
+			y.set(labels.get(_trainData.get(i).label), 0, 1);
+			SimpleMatrix diff_p_y = p.minus(y);
+			SimpleMatrix db2 = p.minus(y);
+
+			SimpleMatrix dU = diff_p_y.mult(h.transpose());
+			dU.plus(lambda, U);
+
+			SimpleMatrix db1 = derivativeTanh(h).elementMult(U.transpose().mult(diff_p_y));
+
+			SimpleMatrix dW = db1.mult(x.transpose());
+			dW.plus(lambda, W);
+
+			SimpleMatrix dx = W.transpose().mult(db1);
+
+//			b3 = b3.plus(-lr, db3);
+//			W2 = W2.plus(-lr, d);	
+			b2 = b2.plus(-lr, db2);
+			U = U.plus(-lr, dU);
+			b1 = b1.plus(-lr, db1);
+			W = W.plus(-lr, dW);
+			x = x.plus(-lr, dx);
+
+			/////////
+			updateWordVectors(_trainData, i, x);
+		}
 	}
-	
+
+	public void test(List<Datum> testData) {
+		//
+		double correct = 0;
+		Datum datum;
+		for (int i = 0; i < testData.size(); i++) {
+			datum = testData.get(i);
+			if (datum.word.equals("<s>") || datum.word.equals("</s>'"))
+				continue;
+
+			String gold = datum.label.equals("O") ? "O" : "I-" + datum.label;
+
+			// make prediction
+			SimpleMatrix x = getTrainingWinodw(testData, i);
+			SimpleMatrix scores = score(x);
+			String predicted = LABELS[getArgMaxIndex(scores)];
+			if (!predicted.equals("O")) {
+				predicted = "I-" + predicted;
+			}
+			if (gold.equals(predicted)) {
+				correct++;
+			}
+		}
+		System.out.println("done" + correct / testData.size());
+		// out.close();
+	}
+
+	private int getArgMaxIndex(SimpleMatrix scores) {
+		int idx = 0;
+		for (int i = 1; i < scores.numRows(); i++) {
+			if (scores.get(i, 0) > scores.get(idx, 0))
+				idx = i;
+		}
+		return idx;
+	}
+
+	private SimpleMatrix score(SimpleMatrix x) {
+		SimpleMatrix h = tanh(W.mult(x).plus(b1));
+		return softmax(U.mult(h).plus(b2));
+	}
+
+	private SimpleMatrix getTrainingWinodw(List<Datum> _trainData, int index) {
+		SimpleMatrix result = new SimpleMatrix(windowSize * wordSize, 1);
+		for (int i = index - windowSize / 2; i <= Math.min(index + windowSize / 2, _trainData.size() - 1); i++) {
+
+			SimpleMatrix wordvec;
+			int row = -1;
+			if (FeatureFactory.wordToNum.containsKey(_trainData.get(i).word))
+				row = FeatureFactory.wordToNum.get(_trainData.get(i).word);
+			else
+				row = FeatureFactory.wordToNum.get("OOV");
+
+			wordvec = FeatureFactory.allVecs.extractVector(true, row);
+
+			result.insertIntoThis((i - (index - windowSize / 2)) * wordSize, 0, wordvec.transpose());
+		}
+
+		return result;
+	}
+
+	private SimpleMatrix tanh(SimpleMatrix x) {
+		for (int i = 0; i < x.numRows(); i++) {
+			for (int j = 0; j < x.numCols(); j++) {
+				x.set(i, j, Math.tanh(x.get(i, j)));
+			}
+		}
+		return x;
+	}
+
+	private SimpleMatrix softmax(SimpleMatrix x) {
+		SimpleMatrix result = new SimpleMatrix(K, 1);
+		double sum = 0;
+		for (int i = 0; i < K; i++) {
+			sum += Math.exp(x.get(i, 0));
+		}
+		for (int i = 0; i < K; i++) {
+			result.set(i, 0, Math.exp(x.get(i, 0)) / sum);
+		}
+		return result;
+	}
+
+	private SimpleMatrix derivativeTanh(SimpleMatrix h) {
+		// SimpleMatrix result = h.copy();
+		SimpleMatrix result = new SimpleMatrix(h);
+		for (int r = 0; r < result.numRows(); r++) {
+			double tanh = result.get(r, 0);
+			result.set(r, 0, 1 - tanh * tanh);
+		}
+		return result;
+	}
+
+	private void updateWordVectors(List<Datum> _trainData, int index, SimpleMatrix x) {
+		for (int i = index - windowSize / 2; i <= index + windowSize / 2; i++) {
+			int num = 0;
+			if (FeatureFactory.wordToNum.containsKey(_trainData.get(i).word))
+				num = FeatureFactory.wordToNum.get((_trainData.get(i).word));
+			else
+				num = FeatureFactory.wordToNum.get(("OOV"));
+			FeatureFactory.allVecs.insertIntoThis(num, 0, x.extractMatrix((i - (index - windowSize / 2)) * wordSize,
+					((i - (index - windowSize / 2)) + 1) * wordSize, 0, 1).transpose());
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		int T = 10;
+		String dataDir = "C:/Users/Eltshan/git/Stanford_CS224N_pa4/data";
+
+		FeatureFactory.readWordVectors(dataDir + "/wordVectors.txt");
+		FeatureFactory.initializeVocab(dataDir + "/vocab.txt");
+		List<Datum> trainData = FeatureFactory.readTrainData(dataDir + "/train");
+		System.out.println(dataDir + "/test");
+		FeatureFactory.readTestData(dataDir + "/dev");
+
+		WindowModel model = new WindowModel(0, 50, 0.01);
+		model.initWeights();
+		new Random();
+		for (int i = 0; i < T; i++) {
+			System.out.println("\nTraining round " + (i + 1));
+			// shuffle list before SGD
+
+			model.train(trainData);
+
+			model.test(trainData);
+		}
+
+	}
 }
